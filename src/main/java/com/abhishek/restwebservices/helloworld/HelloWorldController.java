aad60a56e53b1f4cc9312844d4d2856dcb2c1389
Abhishek Maity <abhishekmaity@hotmail.com>
2022-11-03 23:16:44 +0530

    REST Controller added

---
+++ src/main/java/com/abhishek/restwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.abhishek.restwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*REST API*/
+
+@RestController
+public class HelloWorldController {
+	
+	@GetMapping( path="/hello-world")
+	public String helloWorld() {
+		return "Hello World !";
+	}
+
+}
