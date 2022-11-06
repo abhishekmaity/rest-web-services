394a9ae6331242483d1b4221a195f1f9d17a2999
Abhishek Maity <abhishekmaity@hotmail.com>
2022-11-06 15:46:59 +0530

    using path variable

--- src/main/java/com/abhishek/restwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/abhishek/restwebservices/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.abhishek.restwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /*REST API*/
@@ -13,9 +14,17 @@ public class HelloWorldController {
 		return "Hello World !";
 	}
 	
+	/* Return message in JSON */
+	
 	@GetMapping( path="/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
 	}
 
+	/* Application of Path Variable */
+	
+	@GetMapping( path="/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World ! Its you %s", name));
+	}
 }
