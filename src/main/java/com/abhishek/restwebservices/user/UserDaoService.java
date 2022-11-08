c9735521cfe8c2cbfa4969c572b23809b68c6f97
Abhishek Maity <abhishekmaity@hotmail.com>
2022-11-08 15:13:25 +0530

    User Bean and UserDaoServices added

---
+++ src/main/java/com/abhishek/restwebservices/user/UserDaoService.java
@@ -0,0 +1,26 @@
+package com.abhishek.restwebservices.user;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserDaoService {
+	
+	private static List<User> users =new ArrayList<>();
+	
+	static{
+		
+		users.add(new User(1,"Ram",LocalDate.now().minusYears(30)));
+		users.add(new User(2,"Ganga",LocalDate.now().minusYears(25)));
+		users.add(new User(3,"Mohan",LocalDate.now().minusYears(50)));
+	}
+	
+	public List<User> findALL(){
+		
+		return users;
+	}
+
+}
