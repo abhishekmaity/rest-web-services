3fba2d7f384e1f24f578e399539cfb11e9f6abde
Abhishek Maity <abhishekmaity@hotmail.com>
2022-11-08 23:36:53 +0530

    exception handling HTTP 404 added

--- src/main/java/com/abhishek/restwebservices/user/UserResource.java
+++ src/main/java/com/abhishek/restwebservices/user/UserResource.java
@@ -1,10 +1,16 @@
 package com.abhishek.restwebservices.user;
 
+import java.net.URI;
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -24,7 +30,23 @@ public class UserResource {
 	/* Retrieve a particular User */
 	@GetMapping("/users/{id}")
 	public User retrieveUser(@PathVariable int id){
-		return service.findOne(id);
+		User user =  service.findOne(id);
+		if(user == null)
+			throw new UserNotFoundException("id: "+id);
+		return user;
 	}
+	
+	/* Create a User */
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser(@RequestBody User user){
+		User savedUser = service.save(user);
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+						.pathSegment("/{id}")
+							.buildAndExpand(savedUser
+								.getId()).toUri();
+		
+		return ResponseEntity.created(location ).build();
+	}	
 
 }
